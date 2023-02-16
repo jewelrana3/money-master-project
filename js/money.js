016af0699ce235070e0ad9d7f8eb13f558e9871c
jewelrana3 <juyelrana7752@gmail.com>
2023-02-16 14:11:24 +0600

    side menu

--- js/money.js
+++ js/money.js
@@ -38,4 +38,29 @@ document.getElementById('calculate').addEventListener('click', function () {
     // console.log(secondElementValue)
      const allTotal = totalAmount - incomeValue;
      secondElement.innerText = allTotal;
+
 });
+
+document.getElementById('input-discount').addEventListener('click',function(){
+    const incomeInput = document.getElementById('income-input');
+    const incomeInputSting = incomeInput.value;
+    const incomeValue = parseFloat(incomeInputSting)
+
+    const discountInput = document.getElementById('save-amount');
+    const discountInputSting = discountInput.value;
+    const discountInputValue = parseFloat(discountInputSting);
+    
+    // incomeValue
+    const number1 = discountInputValue / 100;
+    const number2 = incomeValue - (incomeValue * number1);
+    // console.log(number2)
+    // .innerText=number2;
+    const saveText = document.getElementById('save');
+    const saveTextSting = saveText.innerText = number2;
+
+    const remainTotal  = allTotal - number2;
+    console.log(remainTotal)
+   
+})
+    
+
