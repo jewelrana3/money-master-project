5be374364538430c70b7599b29cf988e25bf899d
jewelrana3 <juyelrana7752@gmail.com>
2023-02-16 09:37:47 +0600

    side ber

--- js/money.js
+++ js/money.js
@@ -1,1 +1,41 @@
-console.log('js file')
+document.getElementById('calculate').addEventListener('click', function () {
+    // income input 
+    const incomeInput = document.getElementById('income-input');
+    const incomeInputSting = incomeInput.value;
+    const incomeValue = parseFloat(incomeInputSting)
+
+    // food input
+    const foodInput = document.getElementById('food');
+    const foodInputSting = foodInput.value;
+    const foodValue = parseFloat(foodInputSting);
+    // console.log(foodValue);
+
+    //    rent input 
+    const rentInput = document.getElementById('rent');
+    const rentInputSting = rentInput.value;
+    const rentValue = parseFloat(rentInputSting);
+
+    // utilities input
+    const utilitiesInput = document.getElementById('Utilities');
+    const utilitiesInputSting = utilitiesInput.value;
+    const utilitiesValue = parseFloat(utilitiesInputSting);
+
+    const newNumber = foodValue + rentValue + utilitiesValue;
+    console.log(newNumber)
+
+    // add 3 input 
+    const firstElement = document.getElementById('first-number');
+    const firstInputSting = firstElement.innerText;
+    const firstInputValue = parseFloat(firstInputSting);
+    // console.log(firstInputValue);
+
+    const totalAmount = firstInputValue + newNumber;
+    firstElement.innerText = totalAmount;
+
+    const secondElement = document.getElementById('second-number');
+    const secondElementSting = secondElement.innerText;
+    const secondElementValue = parseFloat(secondElementSting);
+    // console.log(secondElementValue)
+     const allTotal = totalAmount - incomeValue;
+     secondElement.innerText = allTotal;
+});
